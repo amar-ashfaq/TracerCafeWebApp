298d548ad1b04945c18ac10ba1b3183661c1cd20
Amar Ashfaq <42255379+amar-ashfaq@users.noreply.github.com>
2020-11-19 21:37:57 +0000

    Add files via upload

---
+++ Controllers/CustomerController.cs
@@ -0,0 +1,201 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using TracerCafeWebApp.Context;
+using TracerCafeWebApp.Models;
+using PagedList;
+
+namespace TracerCafeWebApp.Controllers
+{
+    public class CustomerController : Controller
+    {
+        private CustomerContext db = new CustomerContext();
+
+        // GET: Customer
+        public ViewResult Index(string sortOrder, string currentFilter, string searchString, int? page) //ViewResult - Return webpage from an action method
+        {
+            ViewBag.CurrentSort = sortOrder;
+            ViewBag.NameSortParm = String.IsNullOrEmpty(sortOrder) ? "name_desc" : ""; //short hand IF statement using ?
+            ViewBag.AgeSortParm = sortOrder == "Age" ? "age_desc" : "Age";
+
+            if (searchString != null)
+            {
+                page = 1;
+            }
+            else
+            {
+                searchString = currentFilter; //currentFilter would be the result string of the searchString
+            }
+
+            ViewBag.CurrentFilter = searchString;
+
+            var customers = from c in db.Customers
+                            select c;
+
+            if (!String.IsNullOrEmpty(searchString))
+            {
+                customers = customers.Where(c => c.LastName.Contains(searchString) || c.FirstName.Contains(searchString));
+            }
+
+            switch (sortOrder)
+            {
+                case "name_desc":
+                    customers = customers.OrderByDescending(c => c.LastName);
+                    break;
+                case "Age":
+                    customers = customers.OrderBy(c => c.Age); //ascending order
+                    break;
+                case "age_desc":
+                    customers = customers.OrderByDescending(c => c.Age);
+                    break;
+                default:
+                    customers = customers.OrderBy(c => c.LastName); //ascending order
+                    break;
+            }
+
+            int pageSize = 3;
+            int pageNumber = page ?? 1; //if page is nullable set pageNumber to 1, otherwise set it to page
+
+            return View(customers.ToPagedList(pageNumber, pageSize));
+        }
+
+        // GET: Customer/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Customer customer = db.Customers.Find(id);
+            if (customer == null)
+            {
+                return HttpNotFound();
+            }
+            return View(customer);
+        }
+
+        // GET: Customer/Create
+        public ActionResult Create() //essentially acts as a redirect
+        {
+            return View();
+        }
+
+        // POST: Customer/Create       
+        [HttpPost]
+        [ValidateAntiForgeryToken] // To protect from overposting attacks and cross-site request forgery attacks
+        public ActionResult Create([Bind(Include = "ID,Title,FirstName,LastName,Age,Address1,Address2,Address3,Address4,Postcode,Telephone")] Customer customer)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    db.Customers.Add(customer);
+                    db.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+            }
+            catch (DataException /* dex */)
+            {
+                ModelState.AddModelError("", "Unable to save changes. Try again, and if the problem persists see your system administrator.");
+            }
+
+            return View(customer);
+        }
+
+        // GET: Customer/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Customer customer = db.Customers.Find(id);
+            if (customer == null)
+            {
+                return HttpNotFound();
+            }
+            return View(customer);
+        }
+
+        // POST: Customer/Edit/5
+        [HttpPost, ActionName("Edit")]
+        [ValidateAntiForgeryToken]
+        public ActionResult EditCustomer(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            Customer customer = db.Customers.Find(id);
+
+            if (TryUpdateModel(customer, "", new string[] { "Title", "FirstName", "LastName", "Age", "Address1", "Address2", "Address3", "Address4", "Postcode", "Telephone" }))
+            {
+                try
+                {
+                    db.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+                catch (DataException /* dex */)
+                {
+                    ModelState.AddModelError("", "Unable to save changes. Try again, and if the problem persists, see your system administrator.");
+                }
+            }
+
+            return View(customer);
+        }
+
+        // GET: Customer/Delete/5
+        public ActionResult Delete(int? id, bool? saveChangesError = false)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            if (saveChangesError.GetValueOrDefault())
+            {
+                ViewBag.ErrorMessage = "Delete failed. Try again, and if the problem persists see your system administrator.";
+            }
+
+            Customer customer = db.Customers.Find(id);
+            if (customer == null)
+            {
+                return HttpNotFound();
+            }
+            return View(customer);
+        }
+
+        // POST: Customer/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            try
+            {
+                Customer customer = db.Customers.Find(id);
+                db.Customers.Remove(customer);
+                db.SaveChanges();
+            }
+            catch (DataException/* dex */)
+            {
+                return RedirectToAction("Delete", new { id = id, saveChangesError = true });
+            }
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ Models/Customer.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace TracerCafeWebApp.Models
+{
+    public class Customer
+    {
+        [Key]
+        public int ID { get; set; }
+
+        [DisplayName("Title")]
+        [Required(ErrorMessage = "Title is required")]
+        [StringLength(20, MinimumLength = 2)]
+        public string Title { get; set; }
+
+        [DisplayName("Firstname")]
+        [Required(ErrorMessage = "Firstname is required")]
+        [StringLength(35, MinimumLength = 2)]
+        public string FirstName { get; set; }
+
+        [DisplayName("Lastname")]
+        [Required(ErrorMessage = "Lastname is required")]
+        [StringLength(35, MinimumLength = 1)]
+        public string LastName { get; set; }
+
+        [DisplayName("Age")]
+        [Required(ErrorMessage = "Age is required")]
+        [Range(1, 130)]
+        public int Age { get; set; }
+
+        [DisplayName("Street")]
+        [Required(ErrorMessage = "Street is required")]
+        [StringLength(60, MinimumLength = 5)]
+        public string Address1 { get; set; }
+
+        [DisplayName("City")]
+        [Required(ErrorMessage = "City is required")]
+        [StringLength(50, MinimumLength = 3)]
+        public string Address2 { get; set; }
+
+        [DisplayName("County")]
+        [Required(ErrorMessage = "County is required")]
+        [StringLength(50, MinimumLength = 5)]
+        public string Address3 { get; set; }
+
+        [DisplayName("Country")]
+        [Required(ErrorMessage = "Country is required")]
+        [StringLength(60, MinimumLength = 5)]
+        public string Address4 { get; set; }
+
+        [DisplayName("Postcode")]
+        [Required(ErrorMessage = "Postcode is required")]
+        [RegularExpression(@"([A-Za-z]{1,2}[0-9]{1,2}[A-Za-z]?[ ]?)([0-9]{1}[A-Za-z]{2})$", ErrorMessage = "Please enter Postcode in correct format")]
+        public string Postcode { get; set; }
+
+        [DisplayName("Telephone")]
+        [Required(ErrorMessage = "Telephone is required")]
+        [RegularExpression(@"((\+44(\s\(0\)\s|\s0\s|\s)?)|0)7\d{3}(\s)?\d{6}$", ErrorMessage = "Please enter Telephone in correct format")]
+        public string Telephone { get; set; }
+    }
+}
